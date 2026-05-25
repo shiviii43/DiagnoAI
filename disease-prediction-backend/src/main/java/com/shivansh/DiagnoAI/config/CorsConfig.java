5e22646269929c54ff494130fc2456e0fc21d516
Shivansh Harbola <92177666+shiviii43@users.noreply.github.com>
2026-05-25 20:22:55 +0530

    Fixed CORS endpoint

--- disease-prediction-backend/src/main/java/com/shivansh/DiagnoAI/config/CorsConfig.java
+++ disease-prediction-backend/src/main/java/com/shivansh/DiagnoAI/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://diagno-ai-app.vercel.app/")
                         .allowedMethods("GET","PUT","POST","DELETE")
                         .allowedHeaders("*");
             }
