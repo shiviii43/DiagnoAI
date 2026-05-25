3df5fcc1297386a7fe43708953f4a1d5c657dbb4
Shivansh Harbola <92177666+shiviii43@users.noreply.github.com>
2026-05-25 20:06:21 +0530

    Fixed the axios URL after Deployment

--- disease-prediction-frontend/src/services/api.js
+++ disease-prediction-frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL:"http://localhost:8080/api",
+    baseURL:"https://diagnoai-backend.onrender.com/api",
     headers:{
         "Content-Type":"application/json"
     }
